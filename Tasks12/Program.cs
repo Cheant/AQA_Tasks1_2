f3d663554588e6f5f493f491556ce85a81fda27f
Anton Cherevach <Anton.Cherevach@teaminternational.com>
2019-12-03 13:20:19 +0200

    Add project

---
+++ Tasks12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Tasks12Library;
+
+namespace Tasks12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Radius: ");
+            Circle circle = new Circle(Console.ReadLine());
+            circle.PrintArea();
+
+            Console.WriteLine("Enter Side: ");
+            Square square = new Square(Console.ReadLine());
+            square.PrintArea();
+
+            InsertShape insertShape = new InsertShape(circle.Area, square.Area);
+            insertShape.PrintResult();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tasks12Library/InsertShape.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tasks12Library
+{
+    public class InsertShape
+    {
+        public double circleArea;
+        public double squareArea;
+
+        public InsertShape(double circlearea, double squarearea)
+        {
+            circleArea = circlearea;
+            squareArea = squarearea;
+        }
+
+        public void PrintResult()
+        {
+            if (Math.Sqrt(circleArea / Math.PI) < Math.Sqrt(squareArea) / 2)
+            {
+                Console.WriteLine($"It is possible to put the circle into the square");
+            }
+            else if (Math.Sqrt(circleArea / Math.PI) > Math.Sqrt(squareArea / 2))
+            {
+                Console.WriteLine($"It is possible to put the square into the circle");
+            }
+            else
+            {
+                Console.WriteLine($"It is impossible to put the circle into the square or the square into the circle");
+            }
+        }
+    }
+}
